325533339421e3a7577797e038068a1ce7c03984
aesthetic-what <ladnoladno477@gmail.com>
2024-07-16 12:08:51 +0500

    module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -2,7 +2,7 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 
 while i != len(my_list):
-    x = my_list.pop()
+    x = my_list.pop(0)
     if x > 0:
         print(x)
     elif x < 0:
